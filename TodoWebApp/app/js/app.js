8d7d5fb6a7f357e81507c9f1effef47dfa3cc71c
Christian Weyer <christian.weyer@thinktecture.com>
2013-02-24 15:44:03 +0100

    Added AJS/BS client

---
+++ TodoWebApp/app/js/app.js
@@ -0,0 +1,12 @@
+﻿var TodoApp = angular.module("TodoApp", ["ngResource"]).
+    config(function ($routeProvider) {
+        $routeProvider.
+            when('/', { controller: ListController, templateUrl: 'list.html' }).
+            when('/new', { controller: CreateController, templateUrl: 'detail.html' }).
+            when('/edit/:itemId', { controller: EditController, templateUrl: 'detail.html' }).
+            otherwise({ redirectTo: '/' });
+    });
+
+TodoApp.factory('Todo', function ($resource, $http) {
+    return $resource('../../api/todos/:id', { id: '@id' }, { update: { method: 'PUT' } });
+});
---
+++ TodoWebApp/app/js/controllers/editController.js
@@ -0,0 +1,10 @@
+﻿var EditController = function ($scope, $routeParams, $location, Todo) {
+    $scope.item = Todo.get({ id: $routeParams.itemId });
+    $scope.action = "Update";
+
+    $scope.save = function () {
+        Todo.update({ id: $scope.item.TodoItemId }, $scope.item, function () {
+            $location.path('/');
+        });
+    };
+};
